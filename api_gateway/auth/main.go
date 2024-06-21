425283ef5597defbf68ebb67b47adafb3b6b0bd6
hiroki-it <hasegawafeedshop@gmail.com>
2024-06-21 17:05:44 +0900

    コメントを追加した

--- api_gateway/auth/main.go
+++ api_gateway/auth/main.go
@@ -9,9 +9,11 @@ func checkAuth(w http.ResponseWriter, r *http.Request) {
 	authString := r.Header.Get("Authorization")
 	if authString == "CSlkjdfj3423lkj234jj==" {
 		w.WriteHeader(http.StatusOK)
+		// Authorizationヘッダーを検証する
 		w.Write([]byte("Authorized: true"))
 		return
 	}
+	// AuthorizationヘッダーにJWTがない場合に、認可エラーとする
 	http.Error(w, "Authorized: false", http.StatusUnauthorized)
 }
 
